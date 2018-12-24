0e05ffd90b7ba34bbc83e19bad71eecaa85f1448
Stephanie Galla <sgalla32@gmail.com>
2018-12-24 13:43:30 +1300

    Tassel5_Avocet.sh

---
+++ Tassel5_Avocet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+'/home/stephanie/Desktop/Rogers_Tassel_Modifications/tassel5-TEGenzymes.v2/tassel5-src/run_pipeline.pl' -Xmx16G -fork1 -GBSSeqToTagDBPlugin -e ApeKI -i '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/' -db '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/2016_2018_Trimmed_ConcatSamples_Kaki.db' -k '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/keyfile_barcoded_concat_samples.txt' -kmerLength 64 -minKmerL 20 -mnQS 30 -mxKmerNum 100000000 -endPlugin -runfork1 
+
+'/home/stephanie/Desktop/Rogers_Tassel_Modifications/tassel5-TEGenzymes.v2/tassel5-src/run_pipeline.pl' -Xmx16G -fork1 -TagExportToFastqPlugin -db '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/2016_2018_Trimmed_ConcatSamples_Kaki.db' -o '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/tagsForAlign.fa.gz' -c 10 -endPlugin -runfork1 
+
+bowtie2 -p 15 --very-sensitive -x '/media/stephanie/Data/Genomes/Kaki_Superscaffolds/Kaki_v2_Concat/Bowtie_Indexed/Kaki1_v2.2_concat_index.fasta' -U '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/tagsForAlign.fa.gz' -S '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/tagsForAlign.sam'
+
+'/home/stephanie/Desktop/Rogers_Tassel_Modifications/tassel5-TEGenzymes.v2/tassel5-src/run_pipeline.pl' -Xmx16G -fork1 -SAMToGBSdbPlugin -i '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/tagsForAlign.sam' -db '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/2016_2018_Trimmed_ConcatSamples_Kaki.db' -aProp 0.0 -aLen 0 -endPlugin -runfork1 
+
+'/home/stephanie/Desktop/Rogers_Tassel_Modifications/tassel5-TEGenzymes.v2/tassel5-src/run_pipeline.pl' -Xmx16G -fork1 -DiscoverySNPCallerPluginV2 -db '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/2016_2018_Trimmed_ConcatSamples_Kaki.db' -sC 1 -eC 1 -mnLCov 0.1 -mnMAF 0.05 -deleteOldData true -endPlugin -runfork1 
+
+'/home/stephanie/Desktop/Rogers_Tassel_Modifications/tassel5-TEGenzymes.v2/tassel5-src/run_pipeline.pl' -Xmx16G -fork1 -ProductionSNPCallerPluginV2 -db '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/2016_2018_Trimmed_ConcatSamples_Kaki.db' -e ApeKI -i '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/' -k '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/keyfile_barcoded_concat_samples.txt' -kmerLength 64 -o '/media/stephanie/Data/GBS/Tassel5/Tassel5_14Aug2018_30Q/Kaki_5Sept2018_ConcatSamples/Kaki_5Sept2018.vcf' -endPlugin -runfork1 
